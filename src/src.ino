5d2675ba76028f806fa360cf8479704d4b5cc27a
Aron Lebani <aron@lebani.dev>
2024-03-30 16:18:37 +1100

    Refactor and tidy up

--- src/src.ino
+++ src/src.ino
@@ -5,44 +5,28 @@
 // (c) Aron Lebani 2024 
 
 #include <MCUFRIEND_kbv.h>
-MCUFRIEND_kbv tft;
 #include <TouchScreen.h>
-
-#include "TimerOne.h"
+#include <TimerOne.h>
 
 #define _DEBUG
 
-const int XP=8,XM=A2,YP=A3,YM=9;	// ID = 0x7575
-const int TS_LEFT=912,TS_RT=104,TS_TOP=75,TS_BOT=901;
+// Pins
+#define XP 8
+#define XM A2
+#define YP A3
+#define YM 9
 
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-TSPoint tp;
+// Calibration data
+#define TS_LEFT 912
+#define TS_RT 104
+#define TS_TOP 75
+#define TS_BOT 901
 
+// Parameters
 #define MINPRESSURE 200
 #define MAXPRESSURE 1000
-
 #define DEBOUNCE_DELAY 100
-
-const uint8_t ORIENTATION = 1;	// LANDSCAPE
-uint16_t box_width;
-uint16_t box_height;
-uint16_t id;
-uint16_t bpm = 94;
-uint16_t xpos;
-uint16_t ypos;
-
-volatile uint8_t current_beat = 0;
-
-uint8_t slots[6][16] = {
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-	{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-};
-
-uint64_t last_toggled = 0;
+#define ORIENTATION 1	// Landscape
 
 // Assign human-readable names to some common 16-bit color values:
 #define BLACK   0x0000
@@ -54,13 +38,52 @@ uint64_t last_toggled = 0;
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-void progress_beat(void) {
-	if (current_beat == 15) {
-		current_beat = 0;
-		return;
-	}
+// Globals
+MCUFRIEND_kbv tft;
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+TSPoint tp;
 
-	current_beat = current_beat + 1;
+uint16_t box_width;
+uint16_t box_height;
+uint16_t id;
+uint16_t bpm = 94;
+uint16_t xpos;
+uint16_t ypos;
+
+uint64_t last_toggled = 0;
+
+uint8_t previous_current_beat = 0;
+volatile uint8_t current_beat = 0;
+
+uint8_t slots[6][16] = {
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+	{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+};
+
+// ---- Initialization functions ----
+
+void initialize_serial(void) {
+	#ifdef _DEBUG
+	Serial.begin(9600);
+	#endif
+}
+
+void initialize_display(void) {
+    id = tft.readID();
+
+    tft.reset();
+    tft.begin(id);
+    tft.setRotation(ORIENTATION);
+    tft.fillScreen(BLACK);
+
+    box_width = tft.width() / 16;
+	box_height = tft.height() / 6;
+
+    tft.fillScreen(BLACK);
 }
 
 void initialize_timer(void) {
@@ -68,20 +91,34 @@ void initialize_timer(void) {
 	Timer1.attachInterrupt(progress_beat);
 }
 
-void initialize_slots() {
-	for (int drum = 0; drum < 6; drum++) {
-		for (int beat = 0; beat < 16; beat++) {
-			int x = beat * box_width;
-			int y = drum * box_height;
-			
-			if (slots[drum][beat] == 1) {
-				tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
-			} else {
-				tft.drawRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
-			}
-		}
+// ---- Drawing functions ----
+
+void draw_screen(void) {
+	for (int beat = 0; beat < 16; beat++) {
+		draw_beat(beat);
 	}
 }
+
+void draw_beat(int beat) {
+	for (int drum = 0; drum < 6; drum++) {
+		draw_slot(drum, beat);
+	}
+}
+
+void draw_slot(int drum, int beat) {
+	int x = beat * box_width;
+	int y = drum * box_height;
+
+	tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLACK);
+
+	if (slots[drum][beat] == 1) {
+		tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
+	} else {
+		tft.drawRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
+	}
+}
+
+// ---- State manipulation ----
 
 void toggle_slot(int drum, int beat) {
 	if (millis() - last_toggled < DEBOUNCE_DELAY) {
@@ -90,18 +127,22 @@ void toggle_slot(int drum, int beat) {
 
 	last_toggled = millis();
 
-	int x = beat * box_width;
-	int y = drum * box_height;
-
-	tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLACK);
-
 	if (slots[drum][beat] == 1) {
 		slots[drum][beat] = 0;
-		tft.drawRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
 	} else {
 		slots[drum][beat] = 1;
-		tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
 	}
+
+	draw_slot(drum, beat);
+}
+
+void progress_beat(void) {
+	if (current_beat == 15) {
+		current_beat = 0;
+		return;
+	}
+
+	current_beat = current_beat + 1;
 }
 
 uint8_t get_previous_beat(void) {
@@ -115,22 +156,18 @@ uint8_t get_previous_beat(void) {
 void clear_previous_beat(void) {
 	uint8_t previous_beat = get_previous_beat();
 
-	Serial.println("current-beat: " + String(current_beat));
-	Serial.println("prev-beat: " + String(previous_beat));
-
-	for (int drum = 0; drum < 6; drum++) {
-		int x = previous_beat * box_width;
-		int y = drum * box_height;
-
-		if (slots[drum][previous_beat] == 1) {
-			tft.fillRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
-		} else {
-			tft.drawRect(x + 1, y + 1, box_width - 2, box_height - 2, BLUE);
-		}
-	}
+	draw_beat(previous_beat);
 }
 
+// ---- Event loop ----
+
 void highlight_current_beat(void) {
+	if (current_beat == previous_current_beat) {
+		return;
+	}
+
+	previous_current_beat = current_beat;
+
 	clear_previous_beat();
 
 	for (int drum = 0; drum < 6; drum++) {
@@ -141,28 +178,7 @@ void highlight_current_beat(void) {
 	}
 }
 
-void setup(void) {
-	#ifdef _DEBUG
-	Serial.begin(9600);
-	#endif
-
-    tft.reset();
-    id = tft.readID();
-    tft.begin(id);
-    tft.setRotation(ORIENTATION);
-    tft.fillScreen(BLACK);
-
-    box_width = tft.width() / 16;
-	box_height = tft.height() / 6;
-    tft.fillScreen(BLACK);
-
-	initialize_slots();
-	initialize_timer();
-}
-
-void loop() {
-	highlight_current_beat();
-
+void wait_for_touch(void) {
     tp = ts.getPoint();
 
     // If sharing pins, need to fix the directions of the touchscreen pins
@@ -179,5 +195,21 @@ void loop() {
 
 	int beat = xpos / box_width;
 	int drum = ypos / box_height;
+
 	toggle_slot(drum, beat);
+}
+
+// ---- Entry point ----
+
+void setup(void) {
+	initialize_serial();
+	initialize_display();
+	initialize_timer();
+
+	draw_screen();
+}
+
+void loop() {
+	highlight_current_beat();
+	wait_for_touch();
 }
